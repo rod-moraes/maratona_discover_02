aa47805d7d5fb1f20e96b49b8865393eaf4f0305
Rodrigo Carvalho Moraes <rodrigomoraes61@outlook.com>
2021-04-08 23:12:10 -0300

    second commit - alteration in index.ejs job-edit.ejs job.ejs profile.ejs and router.js sever.js

--- src/server.js
+++ src/server.js
@@ -2,12 +2,19 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 
+
+// habilita o motor ejs
 server.set("view engine","ejs")
 
-
+//rotas para arquivos estaticos
 server.use(express.static("public"))
+
+//usar o req.body
+server.use(express.urlencoded({ extended: true}))
 
 // request, response
 server.use(routes)
 
+
+//abre a porta 3000 pro servidor
 server.listen(3000,()=> console.log("rodando"))
